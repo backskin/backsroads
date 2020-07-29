c5645631ec98c8e9e39804323712bb673111fc14
backskin <backskin@outlook.com>
2020-07-29 13:20:40 +0400

    I don't know, what am i doing, but I feel great #2

--- src/funny_generators.rs
+++ src/funny_generators.rs
@@ -1,2 +1,60 @@
+use crate::sweet_mod::{CarRawData, GeoPosition, CarDriver};
+use rand::Rng;
 
-fn GenerateCarMovement(function: fn) -> {}
+pub(crate) trait GeoGenerator {
+    fn next_point(&mut self) -> GeoPosition;
+}
+
+pub(crate) enum Direction{
+    Up, UpRight, Right, DownRight, Down, DownLeft, Left, UpLeft,
+}
+
+pub struct LineGenerator {
+    seed: u32,
+    last_x_point: f64,
+    last_y_point: f64,
+    range_value: f64,
+    direction: Direction,
+}
+
+impl LineGenerator{
+    pub fn new(start_x: f64, start_y: f64, range: f64, direction: Direction) -> LineGenerator{
+        LineGenerator{
+            seed: 0,
+            last_x_point: start_x,
+            last_y_point: start_y,
+            range_value: range,
+            direction,
+        }
+    }
+}
+
+impl GeoGenerator for LineGenerator {
+    fn next_point(&mut self) -> GeoPosition {
+        self.last_x_point += 1.0;
+        let y = rand::thread_rng().gen_range(-self.range_value, self.range_value);
+        GeoPosition::new_and_now(self.last_x_point, y)
+    }
+}
+
+pub fn generate_car_movement(meta_generator: &mut dyn GeoGenerator, points_amount: u32) -> CarRawData {
+
+    let some_dude = CarDriver::new("Dude".to_string());
+
+    let mut output = CarRawData::new(some_dude);
+
+    for i in 0..points_amount {
+        let point = meta_generator.next_point();
+        output.add(point);
+    };
+
+    output
+}
+
+pub fn print_out_movement(raw_data: &CarRawData){
+
+    for (index, chunk) in raw_data.get_iterator().enumerate(){
+        let (x, y) = &chunk.coordinate.get_coors();
+        println!("{}-th point: {}, {}, {}", index, x, y, chunk.t_timestamp.elapsed().as_secs());
+    }
+}
--- src/sweet_mod.rs
+++ src/sweet_mod.rs
@@ -1,73 +1,173 @@
 use std::time::{Duration, Instant};
 use std::cmp::Ordering;
+use rand::{Rng};
+use std::slice::Iter;
 
 #[derive(Copy, Clone, PartialEq)]
-struct Coordinate{
+pub struct Coordinate{
     x_latitude: f64,
     y_longitude: f64,
 }
 
-struct TripSegment{
-    start: Coordinate,
-    next: Option<TripSegment>,
+impl Coordinate {
+    pub fn new(x: f64, y: f64) -> Coordinate{
+        Coordinate{
+            x_latitude: x,
+            y_longitude: y,
+        }
+    }
+
+    pub fn get_coors(&self) -> (f64, f64){
+        (self.x_latitude, self.y_longitude)
+    }
 }
 
-trait Human{
-
+pub struct TripSegment{
+    pub start: Coordinate,
+    pub end: Coordinate,
 }
-
-struct CarKey{
+pub struct CarKey{
     key_image: f64,
 }
 
-struct Engine{
-    amount_of_cylinders: u32,
-    performance: f32,
-    ignition_placeholder: f64,
-}
-
-enum SeatState{
-    Vacant,
-    Taken(dyn Human)
-}
-
-struct Seat{
-    state: SeatState,
-}
-
-struct Car{
-    engine: Engine,
-    seats: [Seat; 4],
-}
-
-
-struct CarDriver{
+pub struct CarDriver{
     driver_name: String,
     car_key: CarKey,
 }
 
-impl Human for CarDriver{
+impl CarDriver{
+    pub fn new(name: String)-> CarDriver{
+        let mut rng = rand::thread_rng();
+        let some_random_num: f64 = rng.gen();
+        let key = some_random_num * (rng.gen_range(1000, 1000000) as f64);
+        CarDriver{
+            driver_name: name,
+            car_key: CarKey{ key_image: key },
+        }
+    }
 
 }
 
-struct Trip{
+pub struct Trip{
     segment: TripSegment,
 }
 
-struct MetaRoute{
-    cluster: Vec<Trip>,
-    representative: Trip,
+pub struct MetaRoute{
+    pub cluster: Vec<Trip>,
+    pub representative: Trip,
 }
 
 #[derive(Copy, Clone, PartialEq)]
-struct GeoPosition {
-    coordinate: Coordinate,
-    t_timestamp: Instant,
+pub struct GeoPosition {
+    pub coordinate: Coordinate,
+    pub t_timestamp: Instant,
 }
 
-struct CarData {
-    dude: CarDriver,
+impl GeoPosition{
+    pub fn new(cor: Coordinate, time: Instant) -> GeoPosition{
+        GeoPosition{
+            coordinate: cor,
+            t_timestamp: time,
+        }
+    }
+
+    pub fn new_and_now(x_cor: f64, y_cor: f64)->GeoPosition{
+        GeoPosition{
+            coordinate: Coordinate { x_latitude: x_cor, y_longitude: y_cor },
+            t_timestamp: Instant::now(),
+        }
+    }
+}
+
+pub struct CarRawData{
+    pub dude: CarDriver,
     gps_data: Vec<GeoPosition>
+}
+
+impl CarRawData{
+
+    pub fn new(driver: CarDriver) -> CarRawData{
+        CarRawData{
+            dude: driver,
+            gps_data: vec![],
+        }
+    }
+
+    pub fn remove(&mut self, position: &GeoPosition){
+        let mut index = 0;
+        for i in 0..self.gps_data.len() {
+            if self.gps_data.get(i) == position{
+                index = i;
+                break;
+            }
+        }
+
+        self.gps_data.remove(index);
+    }
+
+    pub fn add(&mut self, position: GeoPosition){
+        self.gps_data.push(position);
+    }
+
+    pub fn get_iterator(&self) -> Iter<'_, GeoPosition> {
+        return self.gps_data.iter();
+    }
+}
+
+struct CarTrips{
+    data: Vec<Trip>
+}
+
+pub struct TripAdvisor{
+    trip_gap: Duration,
+    car_trips: CarTrips,
+}
+
+impl TripAdvisor{
+
+    fn new(raw_data: &CarRawData) -> TripAdvisor {
+
+    }
+
+    fn segments_cleaner(&self){
+
+    }
+
+    fn min_point_trip_filter(&self, threshold: u16){
+
+    }
+
+    fn min_time_trip_filter(&self, threshold: Duration){
+
+    }
+
+    fn centroid_dist_trip_filter(&self, win_size_in_points: u8, threshold: f32){
+
+    }
+
+    fn direction_dyn_trip_filter(&self, threshold: f32){
+
+    }
+
+    fn most_recent_trip_filter(&self){
+
+    }
+
+    fn apply_all_filters(&self){
+        let min_point: u16 = 10;
+        let min_time: Duration = Duration::new(30, 0);
+        let win_size: u8 = 10;
+        let min_dist: f32 = 0.5;
+        let max_ratio: f32 = 0.6;
+
+        self.segments_cleaner();
+        self.min_point_trip_filter(min_point);
+        self.min_time_trip_filter(min_time);
+        self.centroid_dist_trip_filter(win_size, min_dist);
+        self.direction_dyn_trip_filter(max_ratio);
+        self.most_recent_trip_filter();
+    }
+
 }
 
 impl GeoPosition {
